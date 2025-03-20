b59c26001912d56223d1759a75039aba7249430d
LimbergDV <limbergdelvalle@gmail.com>
2025-03-20 17:33:53 -0600

    add: adding changes in principal page

---
+++ src/app/ui/principal/cards-principal/cards-principal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-principal',
+  templateUrl: './cards-principal.component.html',
+  styleUrl: './cards-principal.component.css'
+})
+export class CardsPrincipalComponent {
+
+}
