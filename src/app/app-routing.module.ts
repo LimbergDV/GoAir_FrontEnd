b0678611cd527ab0b4b14cf84007458d75f3631d
LimbergDV <limbergdelvalle@gmail.com>
2025-03-25 22:53:52 -0600

    add: login finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalPageComponent } from './ui/principal/principal-page/principal-page.component';
 import { NotificationsPageComponent } from './ui/notifications/notifications-page/notifications-page.component';
+import { LoginPageComponent } from './ui/login/login-page/login-page.component';
 
 
 const routes: Routes = [
   {path: "home", component: PrincipalPageComponent},
-  {path: "notifications", component: NotificationsPageComponent}
+  {path: "notifications", component: NotificationsPageComponent},
+  {path: "login", component: LoginPageComponent}
 ];
 
 @NgModule({
