85ca73fb1a1b5d59b0b2e472a81ffb8afcfe47a8
LimbergDV <limbergdelvalle@gmail.com>
2025-03-25 23:18:29 -0600

    add: starting with the register of users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrincipalPageComponent } from './ui/principal/principal-page/principal-page.component';
 import { NotificationsPageComponent } from './ui/notifications/notifications-page/notifications-page.component';
 import { LoginPageComponent } from './ui/login/login-page/login-page.component';
+import { RegisterPageComponent } from './ui/register/register-page/register-page.component';
 
 
 const routes: Routes = [
   {path: "home", component: PrincipalPageComponent},
   {path: "notifications", component: NotificationsPageComponent},
-  {path: "login", component: LoginPageComponent}
+  {path: "login", component: LoginPageComponent},
+  {path: "signUp", component: RegisterPageComponent}
 ];
 
 @NgModule({
