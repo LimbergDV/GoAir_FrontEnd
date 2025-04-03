8eefe0d0ec08bd7bec60be522f11f2b9ec46df99
LimbergDV <limbergdelvalle@gmail.com>
2025-04-03 09:09:02 -0600

    add: adding modals of information-place-sensors

--- src/app/ui/modals/modals.module.ts
+++ src/app/ui/modals/modals.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NameOfPlaceComponent } from './name-of-place/name-of-place.component';
 import { FormsModule } from '@angular/forms';
+import { InformationPlaceSensorsComponent } from './information-place-sensors/information-place-sensors.component';
 
 
 
 @NgModule({
   declarations: [
-    NameOfPlaceComponent
+    NameOfPlaceComponent,
+    InformationPlaceSensorsComponent
   ],
   imports: [
     CommonModule,
