f340f3764e87462dcb3c406aa15471050c6728c4
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2025-04-04 10:24:32 -0600

    graficas

--- src/app/ui/statistics/statistics.module.ts
+++ src/app/ui/statistics/statistics.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardsStatisticsComponent } from './cards-statistics/cards-statistics.component';
 import { StatisticsContainerComponent } from './statistics-container/statistics-container.component';
 import { StatisticsPageComponent } from './statistics-page/statistics-page.component';
 import { ChartsModule } from './charts/charts.module';
@@ -9,7 +8,6 @@ import { ChartsModule } from './charts/charts.module';
 
 @NgModule({
   declarations: [
-    CardsStatisticsComponent,
     StatisticsContainerComponent,
     StatisticsPageComponent,
     
